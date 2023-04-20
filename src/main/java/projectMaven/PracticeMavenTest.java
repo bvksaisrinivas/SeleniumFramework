fe848d174b618a0cc8848cf3b14a357f8dcc4ff6
abcd <abcd@DESKTOP-DIFRD3P>
2023-04-20 20:31:26 +0530

    2nd committ

---
+++ src/main/java/projectMaven/PracticeMavenTest.java
@@ -0,0 +1,12 @@
+package projectMaven;
+
+import org.testng.annotations.Test;
+
+public class PracticeMavenTest {
+	@Test
+	
+	public void run() {
+		
+		System.out.println();
+	}
+}
---
+++ src/test/java/demo/Parameters.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Parameters {
+
+	public void m1(){
+		
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+	}
+}
